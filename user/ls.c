b492452d3835ed14226a67ad31c2efdf60ddc4e3
ForsakenDelusion <2430385640cy@gmail.com>
2024-09-11 12:03:45 +0800

    finish find

--- user/ls.c
+++ user/ls.c
@@ -30,33 +30,41 @@ ls(char *path)
   struct dirent de;
   struct stat st;
 
+  //打开目录，为后续操作做准备
   if((fd = open(path, 0)) < 0){
     fprintf(2, "ls: cannot open %s\n", path);
     return;
   }
 
+  //获取刚刚打开目录的信息
   if(fstat(fd, &st) < 0){
     fprintf(2, "ls: cannot stat %s\n", path);
     close(fd);
     return;
   }
-
+  //查看目录信息，区分类别
   switch(st.type){
+  //如果是文件，就打印出信息
   case T_FILE:
     printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
     break;
-
+  //如果是目录，就进行目录处理
   case T_DIR:
+    //判断目录长度
     if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
       printf("ls: path too long\n");
       break;
     }
+    //如果长度没问题，就吧path拷贝到目录缓冲区
     strcpy(buf, path);
+    //指向目录缓冲区中，path的结尾，用于在目录后面加上’/‘
     p = buf+strlen(buf);
     *p++ = '/';
+    //读取文件信息，塞进de里面，用于分离出文件编号和名字
     while(read(fd, &de, sizeof(de)) == sizeof(de)){
       if(de.inum == 0)
         continue;
+      //将文件名写到缓冲区结尾的‘/’后面
       memmove(p, de.name, DIRSIZ);
       p[DIRSIZ] = 0;
       if(stat(buf, &st) < 0){
