b14f3c9247600e1b9bf878f424a118bae6e4c310
ForsakenDelusion <2430385640cy@gmail.com>
2024-09-27 20:49:51 +0800

    pass trace but not sysinfo

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -95,3 +95,22 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+// my trace
+uint64
+sys_trace(void) {
+  int mask;
+  if(argint(0, &mask) < 0) {
+    return -1;
+  };
+  myproc()->mask = mask;
+  return 0;
+}
+
+uint64
+sys_sysinfo(void) {
+  
+  //uint64 freeMem = kfreemem();
+  
+  return 0;
+}
